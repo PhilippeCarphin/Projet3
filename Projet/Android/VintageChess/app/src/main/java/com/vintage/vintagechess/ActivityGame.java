e72b8cae4379e10ef6d9fd381fd8010d1f814aae
Clément Gamache <clement.gamache@polymtl.ca>
2016-11-14 23:26:06 -0500

    refactoring

--- Projet/Android/VintageChess/app/src/main/java/com/vintage/vintagechess/ActivityGame.java
+++ Projet/Android/VintageChess/app/src/main/java/com/vintage/vintagechess/ActivityGame.java
@@ -20,6 +20,7 @@ import android.widget.TextView;
 import org.json.JSONException;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class ActivityGame extends AppCompatActivity {
 
@@ -31,17 +32,18 @@ public class ActivityGame extends AppCompatActivity {
 
     private TextView textViewPlayer1Name, textViewPlayer2Name;
     private TextClock textClockPlayer1, textClockPlayer2;
-    private TextView state;
+    private TextView textViewState;
     private TextView textViewRoundNumber;
     private TextView textViewWhoseTurn;
     private TextView textViewMoveNumber;
     private TextView textViewLocation;
-    
+
     private RadioGroup radioGroupBoardColor;
     private RadioGroup radioGroupPieceStyle;
 
     private Button buttonEndGame;
-    private static boolean gameStarted = false;
+
+    private Game game;
 
     RadioGroup selectStyle;
 
@@ -55,37 +57,16 @@ public class ActivityGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         try {
 
-            Game.activityGame = this;
-            Utilities.currentActivity = this;
-            gameStarted = false;
-
-
-
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_game);
-            textViewRoundNumber = (TextView) findViewById(R.id.textRound);
-            textViewWhoseTurn = (TextView) findViewById(R.id.CurrentPlayer);
-            textViewMoveNumber = (TextView) findViewById(R.id.MoveNumber);;
-            textViewLocation = (TextView) findViewById(R.id.LocationText);;
 
-            board = (ImageView) findViewById(R.id.imageViewBoard);
-            board.setImageResource(android.R.color.transparent);
+            setAllViews();
 
 
-            motionlessPieces = (ImageView) findViewById(R.id.imageViewMotionlessPieces);
-
-            motionlessPieces.setImageResource(android.R.color.transparent);
-
-            movingPiece = (ImageView) findViewById(R.id.imageViewMovingPieces);
-            movingPiece.setImageResource(android.R.color.transparent);
+            Game.activityGame = this;
+            Utilities.currentActivity = this;
 
 
-            board.setOnTouchListener(new View.OnTouchListener() {
-                public boolean onTouch(View v, MotionEvent event) {
-                    handleTouch(event);
-                    return true;
-                }
-            });
 
 
             Display.board = board;
@@ -93,164 +74,10 @@ public class ActivityGame extends AppCompatActivity {
             Display.movingPiece = movingPiece;
 
 
-            state = (TextView)  findViewById(R.id.State);
-            textViewPlayer1Name = (TextView) findViewById(R.id.Player1Name);
-            textViewPlayer2Name = (TextView) findViewById(R.id.Player2Name);
-            textViewLocation = (TextView) findViewById(R.id.LocationText);
 
-            textClockPlayer1 = (TextClock) findViewById(R.id.TimePlayer1);
-            textClockPlayer2 = (TextClock) findViewById(R.id.TimePlayer2);
-            textViewRoundNumber = (TextView) findViewById(R.id.RoundText);
-            textViewWhoseTurn = (TextView) findViewById(R.id.CurrentPlayer);
-            textViewMoveNumber = (TextView) findViewById(R.id.MoveNumber);
-            textViewPlayer1Name.setText(CreateGameInfo.playerName1);
-            textViewPlayer2Name.setText(CreateGameInfo.playerName2);
-            textViewLocation.setText(CreateGameInfo.location);
-            textViewRoundNumber.setText("Round : 0" );
-             //
-            // index = selectStyle.indexOfChild(radioButton)/2;
-
-            radioGroupBoardColor = (RadioGroup) findViewById(R.id.RadioGroupBoardColor);
-            int a = 0;
-            radioGroupBoardColor.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(RadioGroup group, int checkedId) {
-                    handleRadioButtonBoardColorChange();
-                }
-            });
-
-            radioGroupPieceStyle = (RadioGroup) findViewById(R.id.radioGroupStyle);
-            radioGroupPieceStyle.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(RadioGroup group, int checkedId) {
-                    handleRadioButtonStyleChange();
-                }
-            });
-
-             buttonEndGame = (Button) findViewById(R.id.EndGameButton);
-            buttonEndGame.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    handleButtonEndClick();
-                }
-            });
-
-
-
-
-            /*buttonQuit = (Button) findViewById(R.id.QuitGameButton);
-            buttonQuit.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    //handleButtonQuitClick();
-                }
-            });*/
         }
         catch (Exception e) {
             Utilities.messageBox("Failed to create game", e.getMessage());
-        }
-    }
-
-    public void handleRadioButtonStyleChange() {
-
-        int selectedId = radioGroupPieceStyle.getCheckedRadioButtonId();
-        // find the radiobutton by returned id
-        RadioButton tempRadioButton = (RadioButton) findViewById(selectedId);
-
-        int count = radioGroupPieceStyle.getChildCount();
-        ArrayList<RadioButton> listOfRadioButtons = new ArrayList<RadioButton>();
-        for (int i=0;i<count;i++) {
-            View o = radioGroupPieceStyle.getChildAt(i);
-            if (o instanceof RadioButton) {
-                listOfRadioButtons.add((RadioButton)o);
-            }
-        }
-        String[][] resourceNames = new String[][] {{"black_king_1", "white_queen_1"},{"black_king_2", "white_queen_2"}};
-        int i = 0;
-        for (RadioButton r : listOfRadioButtons) {
-            Bitmap imageAndroid = Bitmap.createBitmap(200, 100, Bitmap.Config.ARGB_8888);
-            Canvas tempCanvas = new Canvas(imageAndroid);
-            int backColor = r.isChecked() ? getResources().getColor(R.color.colorAccent) : Color.TRANSPARENT;
-            Paint p = new Paint();
-            p.setColor(backColor);
-            tempCanvas.drawRect(0,0,tempCanvas.getWidth(), tempCanvas.getWidth(), p);
-            for (int j = 0 ; j < 2 ; j++) {
-                Bitmap b = Piece.getResourceBitmap(resourceNames[i][j], tempCanvas.getHeight());
-                tempCanvas.drawBitmap(b, j*tempCanvas.getHeight(), 0, null);
-            }
-
-            r.setBackground(new BitmapDrawable(this.getResources(), imageAndroid));
-            i++;
-        }
-
-        switch(selectedId) {
-            case R.id.radioButtonStyle1:
-                Game.style = "1";
-                break;
-
-            case R.id.radioButtonStyle2:
-                Game.style = "2";
-                break;
-        }
-
-        Display.drawFullBoard();
-    }
-
-    private void handleRadioButtonBoardColorChange() {
-
-        int selectedId = radioGroupBoardColor.getCheckedRadioButtonId();
-        // find the radiobutton by returned id
-
-        switch(selectedId) {
-            case R.id.radioButtonRed:
-                Display.blackSquareColor = BoardColors.redSquareColor;
-                break;
-
-            case R.id.radioButtonBlue:
-                Display.blackSquareColor = BoardColors.blueSquareColor;
-                break;
-
-            case R.id.radioButtonGreen:
-                Display.blackSquareColor = BoardColors.greenSquareColor;
-                break;
-        }
-
-        int count = radioGroupBoardColor.getChildCount();
-        ArrayList<RadioButton> listOfRadioButtons = new ArrayList<RadioButton>();
-        for (int i=0;i<count;i++) {
-            View o = radioGroupBoardColor.getChildAt(i);
-            if (o instanceof RadioButton) {
-                listOfRadioButtons.add((RadioButton)o);
-            }
-        }
-        int i = 0;
-        for (RadioButton r : listOfRadioButtons) {
-            r.setText("");
-            Bitmap imageAndroid = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
-            Canvas tempCanvas = new Canvas(imageAndroid);
-            int backColor = r.isChecked() ? getResources().getColor(R.color.colorAccent) : Color.TRANSPARENT;
-            Paint p = new Paint();
-            p.setColor(backColor);
-            tempCanvas.drawRect(0,0,tempCanvas.getHeight(), tempCanvas.getWidth(), p);
-            int frontColor = i == 0 ? BoardColors.greenSquareColor : (i == 1 ? BoardColors.blueSquareColor : BoardColors.redSquareColor);
-            p.setColor(frontColor);
-            tempCanvas.drawRect((int)(tempCanvas.getHeight()*0.1),(int)(tempCanvas.getHeight()*0.1), (int)(tempCanvas.getHeight()*0.9), (int)(tempCanvas.getHeight()*0.9), p);
-            r.setBackground(new BitmapDrawable(this.getResources(), imageAndroid));
-            i++;
-        }
-
-        Display.drawFullBoard();
-    }
-
-    private void handleButtonEndClick() {
-        try {
-            HttpRunner.runPostGameEnd(null);
-            Intent setIntent = new Intent(this,ActivityMenu.class);
-            startActivity(setIntent);
-
-
-        }
-        catch (Exception e) {
-            //e.printStackTrace();
-            Utilities.messageBox("Error after pressing button", e.getMessage());
         }
     }
 
@@ -258,15 +85,15 @@ public class ActivityGame extends AppCompatActivity {
     public void onWindowFocusChanged(boolean hasFocus) {
         try {
 
-        super.onWindowFocusChanged(hasFocus);
-        board.requestLayout();
-        if (!gameStarted) {
-            Utilities.messageBoxStartGame();
-            gameStarted = true;
-        }
-        Display.drawFullBoard();
+            super.onWindowFocusChanged(hasFocus);
+            board.requestLayout();
+            if (!gameStarted) {
+                Utilities.messageBoxStartGame();
+                gameStarted = true;
+            }
+            Display.drawFullBoard();
 
-        //handleRadioChange();
+            //handleRadioChange();
             handleRadioButtonBoardColorChange();
             handleRadioButtonStyleChange();
 
@@ -303,6 +130,154 @@ public class ActivityGame extends AppCompatActivity {
         super.onDestroy();
     }
 
+    private void setAllViews() {
+        setImageViews();
+        setTextViews();
+        setButtons();
+    }
+
+    private void setImageViews() {
+        board = (ImageView) findViewById(R.id.imageViewBoard);
+        motionlessPieces = (ImageView) findViewById(R.id.imageViewMotionlessPieces);
+        movingPiece = (ImageView) findViewById(R.id.imageViewMovingPieces);
+
+
+        board.setImageResource(android.R.color.transparent);
+        motionlessPieces.setImageResource(android.R.color.transparent);
+        movingPiece.setImageResource(android.R.color.transparent);
+
+
+        board.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent event) {
+                handleTouch(event);
+                return true;
+            }
+        });
+    }
+
+    private void setTextViews() {
+        textViewState = (TextView)  findViewById(R.id.State);
+        textViewPlayer1Name = (TextView) findViewById(R.id.Player1Name);
+        textViewPlayer2Name = (TextView) findViewById(R.id.Player2Name);
+        textViewLocation = (TextView) findViewById(R.id.LocationText);
+        textClockPlayer1 = (TextClock) findViewById(R.id.TimePlayer1);
+        textClockPlayer2 = (TextClock) findViewById(R.id.TimePlayer2);
+        textViewRoundNumber = (TextView) findViewById(R.id.RoundText);
+        textViewWhoseTurn = (TextView) findViewById(R.id.CurrentPlayer);
+        textViewMoveNumber = (TextView) findViewById(R.id.MoveNumber);
+
+        textViewPlayer1Name.setText(CreateGameInfo.playerName1);
+        textViewPlayer2Name.setText(CreateGameInfo.playerName2);
+        textViewLocation.setText(CreateGameInfo.location);
+        textViewRoundNumber.setText("Round : 0" );
+    }
+
+    private void setButtons() {
+        radioGroupBoardColor = (RadioGroup) findViewById(R.id.RadioGroupBoardColor);
+        radioGroupPieceStyle = (RadioGroup) findViewById(R.id.radioGroupStyle);
+        buttonEndGame = (Button) findViewById(R.id.EndGameButton);
+
+        radioGroupBoardColor.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                handleRadioButtonBoardColorChange();
+            }
+        });
+        radioGroupPieceStyle.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                handleRadioButtonStyleChange();
+            }
+        });
+        buttonEndGame.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                handleButtonEndClick();
+            }
+        });
+    }
+
+    public void handleRadioButtonStyleChange() {
+
+        int selectedId = radioGroupPieceStyle.getCheckedRadioButtonId();
+        // find the radiobutton by returned id
+        RadioButton tempRadioButton = (RadioButton) findViewById(selectedId);
+
+        int count = radioGroupPieceStyle.getChildCount();
+        ArrayList<RadioButton> listOfRadioButtons = new ArrayList<RadioButton>();
+        for (int i=0;i<count;i++) {
+            View o = radioGroupPieceStyle.getChildAt(i);
+            if (o instanceof RadioButton) {
+                listOfRadioButtons.add((RadioButton)o);
+            }
+        }
+        String[][] resourceNames = new String[][] {{"black_king_1", "white_queen_1"},{"black_king_2", "white_queen_2"}};
+        int i = 0;
+        for (RadioButton r : listOfRadioButtons) {
+
+            int backColor = r.isChecked() ? getResources().getColor(R.color.colorAccent) : Color.TRANSPARENT;
+            setRadioButtonImage(r, backColor, resourceNames[i][0], resourceNames[i][1]);
+            i++;
+        }
+
+        switch(selectedId) {
+            case R.id.radioButtonStyle1:
+                Game.style = "1";
+                break;
+
+            case R.id.radioButtonStyle2:
+                Game.style = "2";
+                break;
+        }
+
+        Display.drawFullBoard();
+    }
+
+    private void handleRadioButtonBoardColorChange() {
+
+        int selectedId = radioGroupBoardColor.getCheckedRadioButtonId();
+        // find the radiobutton by returned id
+
+        switch(selectedId) {
+            case R.id.radioButtonRed:
+                Display.blackSquareColor = BoardColors.redSquareColor;
+                break;
+
+            case R.id.radioButtonBlue:
+                Display.blackSquareColor = BoardColors.blueSquareColor;
+                break;
+
+            case R.id.radioButtonGreen:
+                Display.blackSquareColor = BoardColors.greenSquareColor;
+                break;
+        }
+
+        LinkedList<RadioButton> listOfRadioButtons = getRadioGroupButtons(radioGroupBoardColor);
+        int i = 0;
+        for (RadioButton r : listOfRadioButtons) {
+
+            int backColor = r.isChecked() ? getResources().getColor(R.color.colorAccent) : Color.TRANSPARENT;
+            int frontColor = i == 0 ? BoardColors.greenSquareColor : (i == 1 ? BoardColors.blueSquareColor : BoardColors.redSquareColor);
+            setRadioButtonImage(r, backColor, frontColor);
+            i++;
+        }
+
+        Display.drawFullBoard();
+    }
+
+    private void handleButtonEndClick() {
+        try {
+            HttpRunner.runPostGameEnd(null);
+            Intent setIntent = new Intent(this,ActivityMenu.class);
+            startActivity(setIntent);
+
+
+        }
+        catch (Exception e) {
+            //e.printStackTrace();
+            Utilities.messageBox("Error after pressing button", e.getMessage());
+        }
+    }
+
     private void handleTouch(MotionEvent event) {
         try {
 
@@ -319,7 +294,6 @@ public class ActivityGame extends AppCompatActivity {
                     Game.handleFingerUp();
                     break;
             }
-
 
         }
         catch (Exception e) {
@@ -338,6 +312,44 @@ public class ActivityGame extends AppCompatActivity {
     public void setWhoseTurn(String turn) {
         Game.isWhiteTurn = turn.equals("1");
         this.textViewWhoseTurn.setText(Game.isWhiteTurn ? "White" : "Black");
+    }
+
+    private void setRadioButtonImage(RadioButton radioButton, int backColor, int frontColor) {
+        Bitmap imageAndroid = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
+        Canvas tempCanvas = new Canvas(imageAndroid);
+        Paint p = new Paint();
+        p.setColor(backColor);
+        tempCanvas.drawRect(0,0,tempCanvas.getHeight(), tempCanvas.getWidth(), p);
+        p.setColor(frontColor);
+        tempCanvas.drawRect((int)(tempCanvas.getHeight()*0.1),(int)(tempCanvas.getHeight()*0.1), (int)(tempCanvas.getHeight()*0.9), (int)(tempCanvas.getHeight()*0.9), p);
+        radioButton.setBackground(new BitmapDrawable(this.getResources(), imageAndroid));
+    }
+
+    private void setRadioButtonImage(RadioButton radioButton, int backColor, String frontResource1, String frontResource2) {
+        Bitmap imageAndroid = Bitmap.createBitmap(200, 100, Bitmap.Config.ARGB_8888);
+        Canvas tempCanvas = new Canvas(imageAndroid);
+        Paint p = new Paint();
+        p.setColor(backColor);
+        tempCanvas.drawRect(0,0,tempCanvas.getWidth(), tempCanvas.getHeight(), p);
+        String[] resourceNames = new String[] {frontResource1, frontResource2};
+        for (int i = 0 ; i < 2 ; i++) {
+            Bitmap b = Piece.getResourceBitmap(resourceNames[i], tempCanvas.getHeight());
+            tempCanvas.drawBitmap(b, i*tempCanvas.getHeight(), 0, null);
+        }
+
+        radioButton.setBackground(new BitmapDrawable(this.getResources(), imageAndroid));
+    }
+
+    private LinkedList<RadioButton> getRadioGroupButtons(RadioGroup radioGroup) {
+        int count = radioGroup.getChildCount();
+        LinkedList<RadioButton> listOfRadioButtons = new LinkedList<RadioButton>();
+        for (int i=0;i<count;i++) {
+            View o = radioGroup.getChildAt(i);
+            if (o instanceof RadioButton) {
+                listOfRadioButtons.add((RadioButton)o);
+            }
+        }
+        return listOfRadioButtons;
     }
 
 }
