871078371d369f7421a277058313ed2e7c5abd08
Clément Gamache <clement.gamache@polymtl.ca>
2016-11-14 19:07:42 -0500

    mis des plus belles couleurs pour les tuiles rouges et bleues

--- Projet/Android/VintageChess/app/src/main/java/com/vintage/vintagechess/BoardColors.java
+++ Projet/Android/VintageChess/app/src/main/java/com/vintage/vintagechess/BoardColors.java
@@ -10,8 +10,8 @@ public class BoardColors {
 
     public static int greenSquareColor = Color.rgb(0x76, 0x96, 0x56);;
     public static int whiteSquareColor = Color.rgb(0xEE, 0xEE, 0xD2);
-    public static int blueSquareColor = Color.rgb(0x00, 0x00, 0xFF);
-    public static int redSquareColor = Color.rgb(0xFF, 0x00, 0x00);
+    public static int blueSquareColor = Color.rgb(92, 106, 153);
+    public static int redSquareColor = Color.rgb(222, 88, 76);
     public static int borderColor = Color.rgb(0x95, 0x5C, 0x3E);
     public static int numberColor = Color.WHITE;
     public static int lastSquareColor = Color.YELLOW;
