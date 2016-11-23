d31e4e3c2dd3457d8828a55eac7655d5ec2daa2d
Sabrina <sabrina.barouche@polymtl.ca>
2016-11-23 12:14:08 -0500

    Ajouts de fonctions utilies pour linteruption + correctif des fonctions pour declancher linteruption sans bloquer le rest du programme, a suivre

--- Projet/serveur/adv7511_zed.sdk/serveur/src/ZedBoard/ZedBoardConfig.c
+++ Projet/serveur/adv7511_zed.sdk/serveur/src/ZedBoard/ZedBoardConfig.c
@@ -6,13 +6,13 @@
 #include "xscugic.h"
 #include "Xil_exception.h"
 #include "test_hdmi.h"
-#define DEBUG
+//#define DEBUG
 //#define TESTING
 #include "debug.h"
 #include "xparameters.h"
 #include "chessboard.h"
 
-//Code inspiré du code de TP1
+//Code inspirÃ© du code de TP1
 
 /*
  * Boutons-poussoirs 1 accessible par le MIO vers le PS
@@ -25,6 +25,7 @@
 static int currentButtonValue;
 static int oldButtonValue = 0;
 static int flag = 0;
+int value = 0;
 
 
 
@@ -38,6 +39,10 @@ XGpioPs_Config *GPIO_MIOConfigPtr;
 // variables pour le gestionnaire d'interruptions GIC
 XScuGic InterruptController;
 XScuGic_Config *GicConfigPtr;
+
+/* variables globales pour le timer */
+XScuTimer_Config *TMRConfigPtr;
+XScuTimer Timer;
 
 /*******************************************************************************
  *Fonction du TP1
@@ -63,17 +68,65 @@ int configGPIOs () {
   return XST_SUCCESS;
 }
 
+/* routine d'interruption */
+void TimerIntrHandler(void *CallBackRef) {
+   /*
+    * juste pour compter de faï¿½on pas tres originale le
+    * nombre de fois qu'on entre dans la routine...
+    */
+   static uint32_t nbr = 0;
+   XScuTimer *TimerInstancePtr = (XScuTimer *) CallBackRef;
+   XScuTimer_ClearInterruptStatus(TimerInstancePtr);
+   WHERE DBG_PRINT("===> Interruption du timer SCU: %d\n\r", nbr++);
+}
+
+/*
+ *  ajustement de la minuterie "proche" du premier ARM
+ *  et du gestionnaire d'interruption
+ */
+void setTimerAndIntr() {
+  /* la base avec le timer */
+  TMRConfigPtr = XScuTimer_LookupConfig( XPAR_PS7_SCUTIMER_0_DEVICE_ID );
+  XScuTimer_CfgInitialize( &Timer,TMRConfigPtr,TMRConfigPtr->BaseAddr );
+  XScuTimer_SelfTest(&Timer);
+
+  /* la base avec le gestionnaire d'interruption */
+  GicConfigPtr = XScuGic_LookupConfig( XPAR_PS7_SCUGIC_0_DEVICE_ID );
+  XScuGic_CfgInitialize(&InterruptController, GicConfigPtr,
+					    GicConfigPtr->CpuBaseAddress);
+  XScuGic_SelfTest( &InterruptController );
+
+  Xil_ExceptionInit();
+  Xil_ExceptionRegisterHandler( XIL_EXCEPTION_ID_IRQ_INT, (Xil_ExceptionHandler)XScuGic_InterruptHandler,&InterruptController);
+  Xil_ExceptionEnable();
+
+  /* connecter le timer au gestionnaire */
+  XScuGic_Connect(&InterruptController, XPAR_SCUTIMER_INTR, (Xil_ExceptionHandler)TimerIntrHandler, &Timer);
+
+  /* enable the interrupt for the Timer at GIC */
+  XScuGic_Enable(&InterruptController, XPAR_SCUTIMER_INTR);
+
+  /* on veut repartir le compteur automatiquement apres chaque interruption */
+  XScuTimer_EnableAutoReload( &Timer );
+  XScuTimer_EnableInterrupt	( &Timer );
+
+  /* Charger le timer et le partir */
+  XScuTimer_LoadTimer(&Timer, TIMER_LOAD_VALUE);
+  XScuTimer_Start(&Timer);
+}
 
 /*******************************************************************************
  *Initialisation du ZedBoard pour le bouton PB1 et LED9
 *******************************************************************************/
 void init_ZedBoard(){
-	 // init_platform_();
-	 configGPIOs();
+	 //init_platform_();
+	setTimerAndIntr();
+	configGPIOs();
+
 }
 
 /*******************************************************************************
- * Fonction de run appelée dans le main
+ * Fonction de run appelÃ©e dans le main
 *******************************************************************************/
 void run_ZedBoard()
 {
@@ -92,8 +145,10 @@ void run_ZedBoard()
 #else
 
 		// TODO RESTART TABLETTE
-		reset_game();
+		//reset_game();
 		WHERE DBG_PRINT("Restart\n");
+		value = XScuTimer_GetCounterValue(&Timer);
+		WHERE DBG_PRINT("bouton PB2 enfoncï¿½ avec beaucoup de rebonds!!! Counter %X\n\r", value);
 
 #endif
 	}
@@ -102,7 +157,7 @@ void run_ZedBoard()
 }
 
 /*******************************************************************************
- * Fonction appelée lors du debut de la game pour allumé la led LED9
+ * Fonction appelÃ©e lors du debut de la game pour allumÃ© la led LED9
 *******************************************************************************/
 void start_game_led()
 {
@@ -110,7 +165,7 @@ void start_game_led()
 }
 
 /*******************************************************************************
- * Fonction appelée lors de la fin de la game pour éteindre la led LED9
+ * Fonction appelÃ©e lors de la fin de la game pour Ã©teindre la led LED9
 *******************************************************************************/
 void end_game_led()
 {
